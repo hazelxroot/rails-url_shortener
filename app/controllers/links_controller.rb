2922220231a16ecdca8170d4360b1e39b0db0f1c
hazelxroot <samh349@gmail.com>
2025-01-12 16:50:05 -0500

    Update UI design

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,7 +1,8 @@
 class LinksController < ApplicationController
-  before_action :set_link, only: [ :show ]
+  before_action :set_link, only: [ :show, :edit, :update, :destroy ]
   def index
     @links = Link.recent_first
+    @link ||= Link.new
   end
 
   def show
@@ -15,6 +16,22 @@ class LinksController < ApplicationController
       index
       render :index, status: :unprocessable_entity
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @link.update(link_params)
+      redirect_to @link
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @link.destroy
+    redirect_to root_path, notice: "Link has been deleted."
   end
 
   private

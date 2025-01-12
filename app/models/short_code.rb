f025161fdef333d7c003ab7003e386b927c8f835
hazelxroot <samh349@gmail.com>
2025-01-12 14:53:39 -0500

    Add ShortCode base62 encoding

---
+++ app/models/short_code.rb
@@ -0,0 +1,22 @@
+class ShortCode
+  ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".freeze
+  BASE = ALPHABET.length
+
+  def self.encode(number)
+    return "0" if number.zero? || number.nil?
+
+    result = ""
+
+    while number > 0 do
+      index = number % BASE
+      char = ALPHABET[index]
+      result.prepend char
+      number /= BASE
+    end
+
+    result
+  end
+
+  def self.decode(string)
+  end
+end
